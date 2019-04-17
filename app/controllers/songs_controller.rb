d4f8f29f6a3587c231727d13bfe8eb8aa11d18cf
Alexandra Palermo <alexmpalermo@gmail.com>
2019-04-17 18:14:32 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -55,7 +55,7 @@ class SongsController < ApplicationController
   private
 
   def song_params
-    params.require(:song).permit()
+    params.require(:song).permit(:title, :genre_name, :artist_name)
   end
 end
 
