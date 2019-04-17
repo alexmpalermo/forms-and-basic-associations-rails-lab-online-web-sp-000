7e503d15c4cda925f6e312389e752c1d59ec26cf
Alexandra Palermo <alexmpalermo@gmail.com>
2019-04-17 18:10:11 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -3,5 +3,11 @@ class Song < ActiveRecord::Base
   belongs_to :genre 
   belongs_to :artist
   
-  
+   def genre_name=(name)
+     self.genre = Genre.find_by(name: name)
+   end
+ 
+   def genre_name
+      self.genre ? self.genre.name : nil
+   end
 end
